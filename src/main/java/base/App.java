7c9d013891cf10ddf7f76a803e48544fe74e1aac
fshofiquel <fshofique@gmail.com>
2021-06-06 21:32:05 -0400

    update

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,6 +3,29 @@ package base;/*
  *  Copyright 2021 Fazlur Shofiquel
  */
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class App
 {
+    public static void main(String[] args)
+    {
+        float c_from;
+        float c_to;
+        float rate;
+        String output;
+
+        Scanner fIn = new Scanner(System.in);
+        Scanner rIn = new Scanner(System.in);
+        DecimalFormat dF = new DecimalFormat("###.##");
+
+        System.out.print("How many euros are you exchanging? ");
+        c_from = fIn.nextFloat();
+        System.out.print("What is the exchange rate? ");
+        rate = rIn.nextFloat();
+
+        c_to = c_from * rate;
+        output = dF.format(c_to)+" U.S. dollars.";
+        System.out.print(output);
+    }
 }
